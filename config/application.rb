a689bbbd08d3586e92e094a68c912eb8a197989f
Stephan Epping <epping@bitside.de>
2020-03-20 16:12:46 +0100

    Add i18n

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Sterilmasks
     config.load_defaults 6.0
 
     config.i18n.default_locale = :de
+    config.i18n.available_locales = [:de, :en]
+    config.i18n.fallbacks = [:en]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
