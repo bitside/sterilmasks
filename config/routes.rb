ae7aa12473559a075846b773d3da7282199f9720
Stephan Epping <epping@bitside.de>
2020-03-20 19:08:59 +0100

    Add pages for impressum, terms and privacy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'dashboard#index'
 
+  get 'impressum', to: 'dashboard#impressum'
+  get 'terms', to: 'dashboard#terms'
+  get 'privacy', to: 'dashboard#privacy'
+
   resources :consumers, only: [:index, :new, :create, :show] do
     member do
       get :confirm_email
