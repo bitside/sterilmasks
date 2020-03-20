8986ce1d622492ef46a73a53cf9c6134ed887c3e
Stephan Epping <epping@bitside.de>
2020-03-20 11:50:07 +0100

    Set dashboard to default root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-  
+  root 'dashboard#index'
+
   resources :consumers, only: [:index, :new, :create, :show]
   resources :providers, only: [:index, :new, :create, :show]
 end
