391280039366f9fcf22efa63ca3673103fd05459
Stephan Epping <epping@bitside.de>
2020-03-20 11:46:58 +0100

    rails generate controller Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  
   resources :consumers, only: [:index, :new, :create, :show]
   resources :providers, only: [:index, :new, :create, :show]
 end
