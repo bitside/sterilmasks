9918fd9dd4326babe96e329fb8e84fe401a023f0
Stephan Epping <epping@bitside.de>
2020-03-20 16:12:58 +0100

    Add validations for provider/consumer

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,3 +1,8 @@
 class Provider < ApplicationRecord
   scope :search, ->(search_term) { where("name ILIKE :wc OR postal_code LIKE :pf OR city LIKE :wc", wc: "%#{search_term}%", pf: "#{search_term}%")}
+  
+  validates :name, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :postal_code, numericality: true
+  validates :city, presence: true
 end
