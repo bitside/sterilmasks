b6ca7a105038bcec25957372011550525c749c5e
Stephan Epping <epping@bitside.de>
2020-03-20 20:30:52 +0100

    Add letter_opener for outgoing mails in development

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def confirmation_email
+    UserMailer.with(user: User.last).confirmation_email
+  end
 end
