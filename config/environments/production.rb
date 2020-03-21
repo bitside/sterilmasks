06c4d34efefe6e9953eda3c899aa0b0ebefb4104
Stephan Epping <epping@bitside.de>
2020-03-21 10:08:00 +0100

    Configure Mailgun in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'sterilmasks.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'sterilmasks.herokuapp.com' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'sterilmasks.heroku.com',
+    :authentication => :plain,
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
